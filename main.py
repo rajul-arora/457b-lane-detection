3f593922af78dcb9b32da69771182d5905b909c9
rajul-arora <rajularora1994@gmail.com>
2017-03-29 23:18:57 -0400

    Working code?

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import math
+import uuid
+
 import cv2
 import numpy as np
 import os
 import sys
-from network.Neuron import Neuron
-from network.NeuralLayer import NeuralLayer
-from network.CustomLayers import ConvolutionLayer
-from network.Network import Network
 from keras.models import Model
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Dropout
+import uuid
 from keras.optimizers import SGD
 from network.Matrix import Matrix
 from network import constants
@@ -18,92 +17,6 @@ from keras.layers import Dense, Activation
 from keras.applications.vgg19 import VGG19
 import itertools
 
-def pool(input: Matrix):
-    """
-    Performs pooling on the input matrix and returns a smaller matrix
-    """
-
-    windowSize = 2
-    stride = 2
-
-    output = []
-    for i in range(0, input.height(), stride):
-
-        outputRow = []
-        for j in range(0, input.width(), stride):
-            maxVal = max(input, windowSize, i, j)
-            outputRow.append(maxVal)
-
-        output.append(outputRow)
-
-    return Matrix.convert(output)
-
-def max(input: Matrix, windowSize, xOffset, yOffset):
-
-    res = input[xOffset][yOffset]
-    for i in range(xOffset, min(windowSize + xOffset, input.height())):
-        for j in range(yOffset, min(windowSize + yOffset, input.width())):
-            if input[i][j] > res:
-                res = input[i][j]
-
-    return res
-
-def avg(input, windowSize, xOffset, yOffset):
-
-    res = 0
-    for i in range(xOffset, windowSize + xOffset):
-        for j in range(yOffset, windowSize + yOffset):
-            res += input[i][j]
-
-    return res / (windowSize * windowSize)
-
-def testConvolution():
-    inputMatrix = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-    ]
-
-    featureMatrix = [
-        [1,0],
-        [0,1],
-    ]
-
-    convOutput = convolution(inputMatrix, featureMatrix)
-
-    for i in range(len(convOutput)):
-        print(convOutput[i])
-
-def testPooling():
-    inputMatrix = [
-        [1,0.2,1,0.3,1],
-        [0.1,0.4,1,0.5,1],
-        [0.2,0.6,0.9,0.7,1],
-        [0.3,0.8,0.8,0.9,1],
-        [0.4,0.9,0.7,0.99,1],
-    ]
-
-    output = pool(inputMatrix)
-
-    for i in range(len(output)):
-        print(output[i])
-
-def testFullyConnected():
-    m1 = [
-        [1,2],
-        [3,4]
-    ]
-
-    m2 = [
-        [5,6],
-        [7,8]
-    ]
-
-    print(fullyConnectedFunction([m1,m2]))
-
-
 def getInputAndOutputMatrices():
     dir = "./lane_images/cordova1_images/"
     files = os.listdir(dir)
@@ -111,16 +24,31 @@ def getInputAndOutputMatrices():
     for file in files:
         yield getPixelMatrices(dir, file)
 
+def normalize(x):
+    x_mins = np.min(x, axis=0)
+    x_maxs = np.max(x, axis=0)
+    x_deltas = x_maxs - x_mins
+    return (x - x_mins) / x_deltas
+
 def generateTrainTestDataSets():
     inputOutputIterator = getInputAndOutputMatrices()
     train = []
     test = []
 
-    for img, output_matrix in itertools.islice(inputOutputIterator, 10):
-        newImg = np.expand_dims(img, axis=2)
-        train.append(newImg)
-        new_output_matrix = np.expand_dims(output_matrix, axis=2)
-        test.append(new_output_matrix)
+    for img, output_matrix in itertools.islice(inputOutputIterator, 20):
+        # newImg = np.expand_dims(img, axis=2)
+        nImg = normalize(img)
+        train.append(nImg)
+        # new_output_matrix = np.expand_dims(output_matrix, axis=2)
+
+        m = [[[] for i in range(0, len(output_matrix[0]))] for j in range(0, len(output_matrix))]
+        for i in range(0, len(output_matrix)):
+            for j in range(0, len(output_matrix[0])):
+                m[i][j].append(output_matrix[i][j])
+                m[i][j].append(output_matrix[i][j])
+                m[i][j].append(output_matrix[i][j])
+
+        test.append(np.array(m))
     return (train, test)
 
 def getPixelMatrices(dir, file):
@@ -145,103 +73,61 @@ def blockshaped(arr, nrows, ncols):
                .swapaxes(1,2)
                .reshape(-1, nrows, ncols))
 
-
-def main():
-
-    # testConvolution()
-    # testPooling()
-    # testFullyConnected()
-
-    # Call Open CV here to get the input
-    # image = [[0 for x in range(16) ] for y in range(16)]
-
-
-    # image = Matrix([16, 16])
-    # for i in range(16):
-    #     image[15 - i][i] = 1
-    #
-    # input = image
-    # print (str(input))
-
-    # datasetIterator = getInputAndOutputMatrices()
-
-    (train_data, test_data) = generateTrainTestDataSets()
-
-    print("[INFO]: Generating Input Feature Models.")
-
-    # for i in range(0,10):
-        # t = train[i]
-
-    # model = VGG19(weights='imagenet', include_top=False)
-    # x_train = []
-    # x_test = []
-    # shape = None
-    # for img, test in itertools.islice(datasetIterator, 10):
-    #     res = model.predict(np.array([img]))[0]
-    #     x_train.append(res)
-    #     x_test.append(test)
-    #     shape = res.shape
-    #
-    # print(shape)
-    # npArray = np.array(x_test)
-    # testData = npArray.transpose().swapaxes(0, 1)
-    # input = Input(shape=shape)
-
-    input_img = Input(shape=(480, 640, 1))  # adapt this if using `channels_first` image data format
-
-    x = Conv2D(16, (3, 3), activation='relu', padding='same', data_format='channels_last')(input_img)
+def segnet(inputImage, filename, shouldSaveWeights=True, shouldTrain=False, train_data=None, test_data=None):
+    print("Entering Segnet")
+    windowSize = (8, 8)
+    dropoutRate = 0.25
+    inputShape = Input(shape=(480, 640, 3))
+    x = Conv2D(32, windowSize, activation='relu', padding='same')(inputShape)
+    x = Dropout(dropoutRate)(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_last')(x)
+    x = Conv2D(16, windowSize, activation='relu', padding='same')(x)
+    x = Dropout(dropoutRate)(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_last')(x)
+    x = Conv2D(8, windowSize, activation='relu', padding='same')(x)
+    x = Dropout(dropoutRate)(x)
     encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-    x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_last')(encoded)
+    x = Conv2D(8, windowSize, activation='relu', padding='same')(encoded)
+    x = Dropout(dropoutRate)(x)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_last')(x)
+    x = Conv2D(16, windowSize, activation='relu', padding='same')(x)
+    x = Dropout(dropoutRate)(x)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation='relu', padding='same', data_format='channels_last')(x)
+    x = Conv2D(32, windowSize, activation='relu', padding='same')(x)
+    x = Dropout(dropoutRate)(x)
     x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same', data_format='channels_last')(x)
-    autoencoder = Model(input_img, decoded)
+    decoded = Conv2D(3, windowSize, activation='sigmoid', padding='same')(x)
+    autoencoder = Model(inputShape, decoded)
     autoencoder.summary()
     autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-    result = autoencoder.fit(np.array(train_data), np.array(test_data), epochs=1)
-    guess = autoencoder.predict(np.expand_dims(train_data[0], axis=0))
 
+    if os.path.isfile(filename):
+        print("Reading File")
+        autoencoder.load_weights(filename)
 
-    # inputOutputIterator = getInputAndOutputMatrices()
-    # partialInputOutputMatrices = []
-    # for wholeImg, output_matrix in inputOutputIterator:
-        # partialInputImages = blockshaped(wholeImg, constants.PARTIAL_IN_IMG_DIM[0], constants.PARTIAL_IN_IMG_DIM[1])
-        # partialOutputMatrices = blockshaped(output_matrix, constants.PARTIAL_IN_IMG_DIM[0], constants.PARTIAL_IN_IMG_DIM[1])
-        # for i in range(len(partialInputImages)):
-        #     partialInputOutputMatrices.append((Matrix.convert(partialInputImages[i]), Matrix.convert(partialOutputMatrices[i])))
+    if shouldTrain and test_data is not None and train_data is not None:
+        print("Training test data")
+        autoencoder.fit(np.array(train_data), np.array(test_data), epochs=1)
 
-    # convLayer = ConvolutionLayer(activation = constants.relu)
-    # # activLayer = NeuralLayer(constants.sigmoid)
-    # poolLayer = NeuralLayer(func = pool)
-    #
-    # layers = [convLayer, poolLayer]
-    # network = Network(layers)
-    #network.train(partialInputImagesAsMatrices[0], [1, 0]) #Just try the first input image segment for now
+    if shouldSaveWeights:
+        autoencoder.save_weights(filename)
+        print("Saved Weights")
 
+    guess = autoencoder.predict(np.expand_dims(inputImage, axis=0))
 
+    for matrix in guess:
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[0])):
+                matrix[i][j][0] = 255 - int(255.0 * matrix[i][j][0])
+                matrix[i][j][1] = 255 - int(255.0 * matrix[i][j][1])
+                matrix[i][j][2] = 255 - int(255.0 * matrix[i][j][2])
+    return guess
 
-    test = Matrix([16, 16])
-    offset = 1
-    # Diagonalise the center
-    for i in range(4, 12):
-        test[15 - i][i] = 1
-    # Off-center the diagonals at the ends
-    for i in range(0, 4):
-        test[15 - i][i + offset] = 1
-    for i in range(12, 16):
-        test[15 - i][i - offset] = 1
-
-    # print ("\nTesting Image (Expecting a 'yes'): " + str(test))
-    # result = network.run(test)
-    # print("CNN Verdict: " + "YES" if result == 1 else "NO")
+def main():
+    (train_data, test_data) = generateTrainTestDataSets()
+    guess = segnet(train_data[0], "weights.h5", shouldTrain=True, test_data=test_data, train_data=train_data)
+    cv2.imwrite('img/test-' + str(uuid.uuid4()) + '.jpg', guess[0])
 
 if __name__ == '__main__':
     main()
